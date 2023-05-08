bc24bcec0ce8e8d1e9921a1bc8d9036e5cdaaaf9
DilbarAkkaya <dilbarmutavalova@gmail.com>
2023-05-08 10:10:48 +0300

    feat: work with event binding

--- angular-basics/src/app/app.component.ts
+++ angular-basics/src/app/app.component.ts
@@ -8,10 +8,21 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'angular-basics';
   number="42";
-  img='https://logowik.com/content/uploads/images/react.jpg';
+  inputValue = '';
+  //img='https://logowik.com/content/uploads/images/react.jpg';
   constructor(){
-    setTimeout(() => {
+   /*  setTimeout(() => {
       this.img='https://logowik.com/content/uploads/images/179_angular.jpg';
-    }, 5000);
+    }, 5000); */
+  }
+  onInput(event: Event){
+    console.log('Event', event);
+    this.inputValue = (<HTMLInputElement>event.target).value;
+  }
+  onClick(event: Event){
+    console.log(event)
+  }
+  onBlur(str: string) {
+    this.inputValue = str;
   }
 }
